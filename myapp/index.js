af1dfa7e70fbd1ef66997d9593f971befdf6c818
tasniaarham <tasniaarhamarham@gmail.com>
2023-04-22 05:33:52 +0600

    extra

--- myapp/index.js
+++ myapp/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const products=require('./products.json')
 const catagory=require('./categories.json')
+const news=require('./news.json')
 const app = express()
 const port = 3000
 
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 })
 app.get('/catagory',(req,res)=>{
   res.send(catagory);
+})
+app.get('/news',(req,res)=>{
+  res.send(news);
+})
+app.get('/news/:id',(req,res)=>{
+  const id=req.params.id;
+  const idwisenews=news.find(index=>index._id==id);
+  res.send(idwisenews);
+})
+
+app.get('/catagory/:id',(req,res)=>{
+  const id=parseInt(req.params.id);
+  if(id==0){
+    res.send(news)
+
+  }
+  else{
+    const catagoryidwisenews=news.filter(index=>index.category_id==id);
+    res.send(catagoryidwisenews);
+  }
+ 
 })
 app.get('/data',(req,res)=>{
     res.send("My name is Tasnia Sharin");
